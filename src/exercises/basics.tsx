a7f26a9376a591a95d43b8b02204c9b69c8c589c
Saadhin <225086954+saadhinnafiz@users.noreply.github.com>
2026-06-25 13:10:50 +0500

    reorganize project structure with exercises folder

---
+++ src/exercises/basics.tsx
@@ -0,0 +1,140 @@
+function Header() {
+  return <h1>Hello React</h1>;
+}
+
+function List() {
+  return (
+    <ul>
+      <li>Lorem ipsum dolor sit amet.</li>
+      <li>Lorem ipsum dolor sit.</li>
+      <li>Lorem ipsum dolor sit amet.</li>
+    </ul>
+  );
+}
+
+function MyButton() {
+  function handleClick() {
+    alert("You Clicked The Button!");
+  }
+  return <button onClick={handleClick}>Click me</button>;
+}
+
+function Button() {
+  return <button>Regular Button</button>;
+}
+
+function BodyText() {
+  return (
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi illum odio
+      cupiditate aspernatur officia sunt suscipit recusandae consequatur
+      assumenda asperiores?
+    </p>
+  );
+}
+
+function OtherList() {
+  return (
+    <ol>
+      <li>React</li>
+      <li>Next.js</li>
+      <li>Larvel</li>
+    </ol>
+  );
+}
+
+function MyAwesomeNavbar() {
+  return (
+    <nav
+      className="navbar navbar-expand-sm navbar-dark bg-dark"
+      aria-label="Third navbar example"
+    >
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          MyAwesomeNavbar
+          <img src="src/assets/react.svg" alt="" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarsExample03"
+          aria-controls="navbarsExample03"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarsExample03">
+          <ul className="navbar-nav me-auto mb-2 mb-sm-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">
+                Disabled
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <form role="search">
+            <input
+              className="form-control"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <MyAwesomeNavbar />
+      <Header />
+      <BodyText />
+      <List />
+      <Button />
+      <OtherList />
+      <MyButton />
+    </div>
+  );
+}
