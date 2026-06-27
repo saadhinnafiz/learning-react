d9dad90a47a66528e83a0433ac3ee1b17834af7f
Saadhin <225086954+saadhinnafiz@users.noreply.github.com>
2026-06-27 20:24:27 +0500

    re-wrote from start - no changes

--- src/exercises/travel-journal/components/Entry.tsx
+++ src/exercises/travel-journal/components/Entry.tsx
@@ -9,21 +9,23 @@ export default function Entry() {
           alt="image of mount fuji"
         />
       </div>
-
       <div className="travel-info-container">
-        <div className="travel-location-row">
-          <img className="map-marker" src={locationPin} alt="map marker pin" />
+        <div className="trvel-location-row">
+          <img src={locationPin} alt="map marker pin" className="map-marker" />
           <span className="travel-country">Japan</span>
         </div>
-        <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
+        <a
+          href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+          className="google-maps-link"
+        >
           View on Google Maps
         </a>
         <h2 className="travel-entry-title">Mount Fuji</h2>
         <p className="travel-trip-dates">12 Jan, 2025 - 24 Jan, 2025</p>
         <p className="travel-entry-text">
           Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
+          // (12,380 feet). Mount Fuji is the single most popular tourist site
+          in // Japan, for both Japanese and foreign tourists.
         </p>
       </div>
     </article>
