7a7089c5277d531a6f69fb0d0ab49f9a929fe4f8
Saadhin <225086954+saadhinnafiz@users.noreply.github.com>
2026-07-02 15:58:57 +0500

    feat: add ingredient list with useState and form submission

--- src/exercises/chef-claude/components/Main.tsx
+++ src/exercises/chef-claude/components/Main.tsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
+
 export default function Main() {
+  const [ingredients, setIngredients] = useState<string[]>([]);
+  const ingredientsListItem = ingredients.map((ingredient) => (
+    <li key={ingredient}> {ingredient}</li>
+  ));
+
+  function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    if (!newIngredient) return;
+    setIngredients((prev) => [...prev, newIngredient as string]);
+    event.currentTarget.reset();
+  }
+
   return (
     <main>
-      <form className="ingredient-form">
+      <form onSubmit={handleSubmit} className="ingredient-form">
         <input
           type="text"
           placeholder="e.g Oregano"
           aria-label="Add ingredient"
           className="ingredient-input-field"
+          name="ingredient"
           autoFocus
         />
-        <button className="ingredient-button">+ Add ingredient</button>
+        <button type="submit" className="ingredient-button">
+          + Add ingredient
+        </button>
       </form>
+      <ul>{ingredientsListItem}</ul>
     </main>
   );
 }
